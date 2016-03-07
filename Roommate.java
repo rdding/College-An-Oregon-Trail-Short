d4196800f61fa93759df3baa2d26edc95275e747
rdding <rddding@gmail.com>
2016-03-06 20:01:34 -0800

    Player and Roommate classes
    Needs javadocs

---
+++ Roommate.java
@@ -0,0 +1,28 @@
+/**
+* Creates a roommate.
+*
+*@author Richard Ding
+*@version program7
+*/
+
+public class Roommate {
+    private String name;
+    private boolean isAlive;
+    
+    public Roommate(String name, boolean isAlive) {
+        this.name = name;
+        this.isAlive = isAlive;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public boolean getIsAlive(){
+        return isAlive;
+    }
+    
+    public void isDead(){
+        isAlive = false;
+    }
+}
