169a8cb842ae48ddf324b3d97c426d0a744c3dc6
AalokAhluwalia <aalokahluwalia@gmail.com>
2016-03-09 09:38:27 -0800

    help git hub plsss

--- Studying.java
+++ Studying.java
@@ -1,6 +1,11 @@
 
 public class Studying implements Minigame{
 
+   private Player p;
+   public Studying(Player p){
+      this.p = p;
+   }
+
    @Override
    public void start() {
       // TODO Auto-generated method stub
