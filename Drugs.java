63dc1595b0b5c5571808aae96285bd7c760665c6
AalokAhluwalia <aalokahluwalia@gmail.com>
2016-03-10 17:33:04 -0800

    just prints 2 txt files in start

--- Drugs.java
+++ Drugs.java
@@ -1,16 +1,33 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Drugs implements Minigame{
 
    @Override
    public void start() {
-      // TODO Auto-generated method stub
-      
+      File f1 = new File("drugs1.txt");
+      File f2 = new File("drugs2.txt");
+      try{
+         FileReader fR = new FileReader(f1);
+         int c = fR.read();
+         while(c != -1){
+            System.out.print((char)c);
+            c = fR.read();
+         }
+         fR = new FileReader(f2);
+         c = fR.read();
+         while(c != -1){
+            System.out.print((char)c);
+            c = fR.read();
+         }
+      }catch(Exception ex){
+         System.out.println(ex);
+      }     
    }
 
    @Override
-   public void exit() {
-      // TODO Auto-generated method stub
-      
+   public void exit() {//no code in play
    }
 
 }
