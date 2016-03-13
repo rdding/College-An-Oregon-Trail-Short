7b165dec70e5b3003d75653229225fe5cd2d7f00
rdding <rddding@gmail.com>
2016-03-13 13:07:09 -0700

    Delete MyTestMain.java

--- MyTestMain.java
+++
@@ -1,8 +0,0 @@
-
-public class MyTestMain {
-   public static void main(String [] args){
-      Player p = new Player(10, 1000, "Larry", null);
-      Blackjack b = new Blackjack(p);
-      b.start();
-   }
-}
